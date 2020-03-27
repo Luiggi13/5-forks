9f5b6499db1e7e9e647b3d4c269b78a905c65267
Christian Llansola <cllansola@strategying.com>
2020-03-27 13:23:34 +0100

    design implemented on account

--- app/screens/Account/UserGuest.js
+++ app/screens/Account/UserGuest.js
@@ -1,11 +1,71 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Text, StyleSheet, View, ScrollView, Image } from 'react-native';
+import { Button } from 'react-native-elements';
 
 
 export default function UserGuest() {
     return (
-        <View>
-            <Text>User guest</Text>
-        </View>
+        <ScrollView style={styles.viewBody} centerContent={true} contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: 'center'
+        }}>
+            <Image
+                source={require("../../../assets/img/user-guest.jpg")}
+                style={styles.image}
+                resizeMode="contain"
+            />
+            <Text style={styles.title}>
+                Consulta tu perfil
+             </Text>
+
+            <Text style={styles.description}>
+                ¿Como describirías tu mejor restaurante? Busca y visualiza los mejores
+                restaurantes de una forma sencilla, vota cual te ha gustado más y
+                comenta como ha sido tu experiencia.
+            </Text>
+            <View style={styles.viewBtn}>
+                <Button
+                    buttonStyle={styles.btnStyle}
+                    containerStyle={styles.btnContainer}
+                    title="Ver tu perfil"
+                    onPress={ () => console.log('ver perfil')}
+                />
+            </View>
+        </ScrollView>
     )
 }
+
+
+const styles = StyleSheet.create({
+    viewBody: {
+        marginLeft: 30,
+        marginRight: 30
+    },
+    image: {
+        height: 300,
+        width: "100%",
+        marginBottom: 40,
+
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 19,
+        marginBottom: 10,
+        textAlign: "center"
+    },
+    description: {
+        textAlign: "center",
+        marginBottom: 20,
+
+    },
+    viewBtn: {
+        flex: 1,
+        alignItems: "center"
+    },
+    btnStyle: {
+        backgroundColor: "#00a680"
+      },
+      btnContainer: {
+        width: "70%"
+      }
+})
