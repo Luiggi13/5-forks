153678bfce9798b41cdb8acdc008cd1daa2d1866
Christian Llansola <cllansola@strategying.com>
2020-03-27 13:56:33 +0100

    Login page for form

---
+++ app/screens/Account/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
+
+export default function Login(){
+    return (
+        <View>
+            <Text>
+                Form Login
+            </Text>
+        </View>
+    )
+}
