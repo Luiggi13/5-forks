45efd7d99d01d68e631f922fc4fad2a680b8e38d
Christian Llansola <cllansola@strategying.com>
2020-03-27 18:10:05 +0100

    register local component

--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -4,29 +4,43 @@ import { Divider } from "react-native-elements";
 import { withNavigation } from "react-navigation";
 
 
-export default function Login(){
-    
+export default function Login(props) {
+
+    const { navigation } = props;
+
     return (
         <ScrollView>
-        <Image source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
-        style={styles.logo}
-        resizeMode="contain"
-    />       
-    <View
-    style={styles.viewContainer}
-    >
-        <Text>login form inside</Text>
-        <Text>Create account</Text>
-    </View>
-    <Divider
-    style={styles.divider}
-    />
+            <Image source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+                style={styles.logo}
+                resizeMode="contain"
+            />
+            <View
+                style={styles.viewContainer}
+            >
+                <Text>login form inside</Text>
+                {/* <Text>Create account</Text> */}
+                <CreateAccount navigation={navigation} />
+            </View>
+            <Divider
+                style={styles.divider}
+            />
 
-    <View style={styles.viewContainer}>
-        <Text>login facebook</Text>
-    </View>
-    </ScrollView>
+            <View style={styles.viewContainer}>
+                <Text>login facebook</Text>
+            </View>
+        </ScrollView>
     );
+}
+
+function CreateAccount(props) {
+    const { navigation } = props;
+    return <Text style={styles.textRegister}>
+        ¿Aún no tienes una cuenta? {" "}
+        <Text style={styles.btnRegister}
+        onPress={()=> navigation.navigate("Register")}>
+            Regístrate
+        </Text>
+    </Text>
 }
 
 
@@ -37,8 +51,17 @@ const styles = StyleSheet.create({
         marginTop: 20
     },
     viewContainer: {
-        marginRight:40,
-        marginLeft:40
+        marginRight: 40,
+        marginLeft: 40
+    },
+    textRegister: {
+        marginTop: 15,
+        marginLeft: 10,
+        marginRight: 10
+    },
+    btnRegister: {
+        color: "#00a680",
+        fontWeight: "bold"
     },
     divider: {
         backgroundColor: "#00a680",
