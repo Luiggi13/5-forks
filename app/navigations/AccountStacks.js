3bb8a37cc057948e593db849007a96507bb5ca0d
Christian Llansola <cllansola@strategying.com>
2020-03-27 10:47:19 +0100

    account added

---
+++ app/navigations/AccountStacks.js
@@ -0,0 +1,15 @@
+import { createStackNavigator } from "react-navigation-stack";
+import MyAccountScreen from "../screens/MyAccount";
+
+const AccountScreenStacks = createStackNavigator(
+    {
+        MyAccount:{
+            screen: MyAccountScreen,
+            navigationOptions: () => ({
+                title: "Mi cuenta"
+            })
+        }
+    }
+)
+
+export default AccountScreenStacks;
