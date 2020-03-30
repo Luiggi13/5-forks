f1749c834c7d6c1bc41b8832bc45fe5de5a42cd0
Christian Llansola <cllansola@strategying.com>
2020-03-30 14:02:00 +0200

    basic sign out from firebase

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Text, View } from 'react-native';
+import { Button } from "react-native-elements";
+
+import * as firebase from "firebase";
 
 export default function UserLogged() {
     return (
         <View>
             <Text>usuario logueado</Text>
+            <Button
+                title="Cerrar sesión"
+                onPress={() => { firebase.auth().signOut()}}
+            />
         </View>
     )
 }
