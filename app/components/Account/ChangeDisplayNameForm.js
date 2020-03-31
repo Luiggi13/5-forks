cc3f2663e17b837f07c0ed8662f34de14fc3df69
Christian Llansola <cllansola@strategying.com>
2020-03-31 20:18:32 +0200

    form displayname

--- app/components/Account/ChangeDisplayNameForm.js
+++ app/components/Account/ChangeDisplayNameForm.js
@@ -1,11 +1,54 @@
 import React from 'react'
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Input, Button } from "react-native-elements";
 import * as firebase from "firebase";
 export default function ChangeDisplayNameForm() {
+
+
+    
+    const updateDisplayName = () => {
+        console.log("Nombre actualizado");
+    }
+
     return (
-        <View>
-            <Text> ChangeDisplayNameForm </Text>
-        </View>
-    )
+      <View style={styles.view}>
+          <Input 
+          placeholder="Nombre"
+          containerStyle={styles.input}
+          rightIcon= {{
+              type: "material-community",
+              name: "account-circle-outline",
+              color: "#c2c2c2"
+          }}
+        //   errorMessage={}
+        //   onChange={}
+        //   defaultValue=""
+          />
+          <Button
+        title="Cambiar nombre"
+        containerStyle={styles.btnContainer}
+        buttonStyle={styles.btn}
+        onPress={updateDisplayName}
+        // loading={isLoading}
+      />
+      </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    view: {
+        alignItems: "center",
+        paddingBottom: 10,
+        paddingTop: 10,
+    },
+    input: {
+        marginBottom: 10
+    },
+    btnContainer: {
+        marginTop: 20,
+        width: "95%"
+      },
+      btn: {
+        backgroundColor: "#ff5c39"
+      }
+});
