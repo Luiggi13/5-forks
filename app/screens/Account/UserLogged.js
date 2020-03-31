f67ae22a7433e4bb053ac33463c449eb7c0a2bed
Christian Llansola <cllansola@strategying.com>
2020-03-31 19:19:03 +0200

    styles

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import InfoUser from "../../components/Account/InfoUser"
-import { Text, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Button } from "react-native-elements";
 import Toast from "react-native-easy-toast";
 import Loading from "../../components/Loading";
+import AccountOptions from "../../components/Account/AccountOptions";
+
 import * as firebase from "firebase";
 
 export default function UserLogged() {
@@ -24,14 +26,17 @@ export default function UserLogged() {
     
     return (
         <View>
-            <InfoUser 
+            <InfoUser style={styles.viewUserInfo}
             userInfo={userInfo}
             setReloadData={setReloadData}
             toastRef={toastRef}
             setIsLoading={setIsLoading}
             setTextLoading={setTextLoading}
             />
+            <AccountOptions />
             <Button
+            buttonStyle={styles.btnCloseSession}
+            titleStyle={styles.btnCloseSessionText}
                 title="Cerrar sesión"
                 onPress={() => { firebase.auth().signOut()}}
             />
@@ -40,3 +45,24 @@ export default function UserLogged() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    viewUserInfo: {
+        minHeight: "100%",
+        backgroundColor: "#f2f2f2"
+      },
+      btnCloseSession: {
+        marginTop: 30,
+        borderRadius: 0,
+        backgroundColor: "#df0000",
+        borderTopWidth: 1,
+        borderTopColor: "#e3e3e3",
+        borderBottomWidth: 1,
+        borderBottomColor: "#e3e3e3",
+        paddingTop: 10,
+        paddingBottom: 10
+      },
+      btnCloseSessionText: {
+        color: "#fff"
+      }
+  });
