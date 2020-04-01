9bd4dedce026cb213776719ac7e541643e7b988c
Christian Llansola <cllansola@strategying.com>
2020-04-02 00:11:08 +0200

    add images, remove images

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -1,35 +1,88 @@
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, View, ScrollView, Alert, Dimensions  } from "react-native";
 import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
 
 export default function AddRestaurantForm(props) {
-    const { navigation,setIsLoading, toastRef} = props;
-const [imagesSelected, setImagesSelected] = useState([]);
+    const { navigation, setIsLoading, toastRef} = props;
+    const [imagesSelected, setImagesSelected] = useState([]);
     
     return (
         <ScrollView>
-            <UploadImage imagesSelected={imagesSelected} setImagesSelected={setImagesSelected} />
+            <UploadImage toastRef={toastRef} imagesSelected={imagesSelected} setImagesSelected={setImagesSelected} />
         </ScrollView>
     )
 }
-
 function UploadImage(props) {
-    const { imageSelected, setImagesSelected } = props;
+    const { imagesSelected, setImagesSelected, toastRef } = props;
+  
+    const imageSelect = async () => {
+      const resultPermission = await Permissions.askAsync(
+        Permissions.CAMERA_ROLL
+      );
+      const resultPermissionCamera =
+        resultPermission.permissions.cameraRoll.status;
+  
+      if (resultPermissionCamera === "denied") {
+        toastRef.current.show(
+          "Es necesario aceptar los permisos de la galeria, si los has rechazado tienes que ir ha ajustes y activarlos manualmente.",
+          3000
+        );
+      } else {
+        const result = await ImagePicker.launchImageLibraryAsync({
+          allowsEditing: true,
+          aspect: [4, 3]
+        });
+  
+        if (result.cancelled) {
+          toastRef.current.show(
+            "Has cerrado la galeria sin seleccionar ninguna imagen",
+            2000
+          );
+        } else {
+          setImagesSelected([...imagesSelected, result.uri]);
+          
+        }
+    }
+};
 
+    const removeImage = image => {
+
+        const arrayImages = imagesSelected;
+        Alert.alert(
+            "Eliminar imagen",
+            "¿Seguro que quieres eliminar la imagen?",
+            [
+                {
+                    text: "Cancel",
+                    style: "cancel"
+                },
+                {
+                    text: "Eliminar",
+                    onPress: ()=> setImagesSelected(arrayImages.filter(imageUrl => imageUrl !== image))
+                    
+                },
+            ],
+            {cancelable: false}
+        )
+
+    }
     return (
         <View style={styles.viewImages}>
-            <Icon
-                type="material-community"
-                name="camera"
-                color="#7a7a7a"
-                containerStyle={styles.containerIcon}
-                onPress={()=> console.log('asd')}
-            />
-            <Avatar
-                onPress={()=>console.log('eliminar')}
-                style={styles.thumbnail}
-                // source={{rul thumbnail rest}}
-            />
+
+            {imagesSelected.length < 5 && (
+                <Icon
+                    type="material-community"
+                    name="camera"
+                    color="#7a7a7a"
+                    containerStyle={styles.containerIcon}
+                    onPress={imageSelect}
+                />
+            )}
+            {imagesSelected.map(image => (
+                <Avatar key={image} onPress={() => removeImage(image)} style={styles.thumbnail} source={{uri: image}} />
+            ))}
         </View>
     )
 }
@@ -37,7 +90,7 @@ function UploadImage(props) {
 const styles = StyleSheet.create({
     viewImages: {
         flexDirection: "row",
-        marginLeft: 20,
+        marginLeft: 10,
         marginRight: 20,
         marginTop: 20
     },
