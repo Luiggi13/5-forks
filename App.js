3a8f9555e686015396c0210c98c317bfa01b53ad
Christian Llansola <cllansola@strategying.com>
2020-04-01 14:25:42 +0200

    code to remove warnings activate for prod

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navigation from './app/navigations/Navigation';
 import { firebaseApp } from "./app/utils/Firebase";
 import { YellowBox } from "react-native";
-
+// console.disableYellowBox = true;
 YellowBox.ignoreWarnings(["Setting a timer"]);
 
 export default function App() {
