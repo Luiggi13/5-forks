866c4a0683f6ae1997552e266e4638bb3fcaaeb0
Christian Llansola <cllansola@strategying.com>
2020-04-01 13:36:17 +0200

    action button added

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react'
+import { Text, View, StyleSheet } from 'react-native';
+import ActionButton from 'react-native-action-button';
+import * as firebase from "firebase";
 
-export default function Restaurants() {
+export default function Restaurants(props) {
+    const { navigation } = props;
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged(userInfo => {
+            setUser(userInfo);
+        });
+    }, [])
+
     return (
-        <View>
+        <View style={styles.viewBody}>
             <Text>Estamos en restaurantesd</Text>
+            {user && <AddRestaurantButton navigation={navigation}/>}
         </View>
     )
 }
+
+function AddRestaurantButton(props) {
+    const { navigation } = props;
+    return (
+        <ActionButton 
+        buttonColor="#00a680"
+        onPress={()=>navigation.navigate("AddRestaurant")}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+   viewBody: {
+       flex:1
+   } 
+});
