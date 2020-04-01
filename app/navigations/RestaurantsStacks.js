b9606da4d6b26aab1931cdda2f2e362efef906bb
Christian Llansola <cllansola@strategying.com>
2020-04-01 12:53:26 +0200

    screens for restaurant

--- app/navigations/RestaurantsStacks.js
+++ app/navigations/RestaurantsStacks.js
@@ -1,5 +1,6 @@
 import { createStackNavigator } from "react-navigation-stack";
 import RestaurantsScreen from "../screens/Restaurants";
+import AddRestaurantScreen from "../screens/Restaurants/AddRestaurant";
 
 const RestaurantsScreenStacks = createStackNavigator(
     {
@@ -7,6 +8,12 @@ const RestaurantsScreenStacks = createStackNavigator(
             screen: RestaurantsScreen,
             navigationOptions: () => ({
                 title: "Restaurantes"
+            })
+        },
+        AddRestaurant:{
+            screen: AddRestaurantScreen,
+            navigationOptions: () => ({
+                title: "Nuevo restaurante"
             })
         }
     }
---
+++ app/screens/Restaurants/AddRestaurant.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function AddRestaurant() {
+    return (
+        <View>
+            <Text>Add a restaurant</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+  });
