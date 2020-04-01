9c520ff3bc7b2cd48ed16aee2dd04e6fc751d0dd
Christian Llansola <cllansola@strategying.com>
2020-04-01 16:11:21 +0200

    navigation,setIsLoading, toastRef to child (form)

---
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, View, Text } from "react-native";
+
+export default function AddRestaurantForm(props) {
+    const { navigation,setIsLoading, toastRef} = props;
+    
+    return (
+        <View>
+            <Text>Add AddRestaurantForm</Text>
+        </View>
+    )
+}
