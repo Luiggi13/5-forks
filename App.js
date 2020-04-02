f3fd73a9b6a16e41f0b723442e03981bff94844d
Christian Llansola <cllansola@strategying.com>
2020-04-02 19:35:54 +0200

    no work with uuid

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Navigation from './app/navigations/Navigation';
 import { firebaseApp } from "./app/utils/Firebase";
 import { YellowBox } from "react-native";
 // console.disableYellowBox = true;
-YellowBox.ignoreWarnings(["Setting a timer"]);
+// YellowBox.ignoreWarnings(["Setting a timer"]);
 
 export default function App() {
   return <Navigation />;
