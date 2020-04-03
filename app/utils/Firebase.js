f28c9cb3b0700a88ab7dbcae784cdf1be3a63c58
Christian Llansola <cllansola@strategying.com>
2020-04-03 11:30:39 +0200

    nulls

--- app/utils/Firebase.js
+++ app/utils/Firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAp-ssSHWN7_ElVSfbDnCoAGRLx6k18Tnc",
-    authDomain: "tenedores-7f552.firebaseapp.com",
-    databaseURL: "https://tenedores-7f552.firebaseio.com",
-    projectId: "tenedores-7f552",
-    storageBucket: "tenedores-7f552.appspot.com",
-    messagingSenderId: "367014555627",
-    appId: "1:367014555627:web:0c663fc8f6ab09113333ae"
+    apiKey: null,
+    authDomain: null,
+    databaseURL: null,
+    projectId: null,
+    storageBucket: null,
+    messagingSenderId: null,
+    appId: null
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -22,5 +22,5 @@ export function create_UUID(){
     });
     return uuid;
   }
-  
+
   
